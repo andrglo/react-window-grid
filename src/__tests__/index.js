c7c37edf408b538ae08bceb72b2e912a64a2a62d
Andre Gloria <andredagloria@gmail.com>
2019-05-09 16:45:34 -0300

    Use react-testing-library

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,40 +1,70 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {act} from 'react-dom/test-utils'
+import {
+  render,
+  cleanup
+  // prettyDOM
+} from 'react-testing-library'
+
 import ReactWindowGrid from '../..'
 
-let container
+afterEach(cleanup)
 
-beforeEach(() => {
-  container = document.createElement('div')
-  document.body.appendChild(container)
-})
+const getStyle = container => container.getAttribute('style')
 
-afterEach(() => {
-  document.body.removeChild(container)
-  container = null
-})
+const tests = [
+  [
+    'Auto calculate height and width',
+    {
+      id: 'test1',
+      height: 50,
+      width: 100,
+      columns: [
+        {id: 'column1', label: 'Column 1'},
+        {id: 'column2', label: 'Column 2'}
+      ],
+      recordset: [
+        {column1: 'cell l/c 1/1', column2: 'cell 1/2'},
+        {column1: 'cell 2/1', column2: 'cell 2/2'},
+        {column1: 'cell 3/1', column2: 'cell 3/2'}
+      ],
+      rowHeaderWidth: 10
+    }
+  ]
+]
 
-it('can render', () => {
-  act(() => {
-    ReactDOM.render(
-      <ReactWindowGrid
-        height={100}
-        width={100}
-        columns={[
-          {id: 'column1', label: 'Column 1'},
-          {id: 'column2', label: 'Column 2'}
-        ]}
-        recordset={[
-          {column1: 'cell 1/1', column2: 'cell 1/2'},
-          {column1: 'cell 2/1', column2: 'cell 2/2'},
-          {column1: 'cell 3/1', column2: 'cell 3/2'}
-        ]}
-        rowHeaderWidth={10}
-      />,
-      container
-    )
-  })
-  const div = container.querySelector('div')
-  expect(div.textContent).toBe('Column 1Column 2123cell 1/1cell 1/2cell 2/1cell 2/2cell 3/1cell 3/2')
+test(tests[0][0], async () => {
+  const props = tests[0][1]
+  const {container, getAllByText, rerender} = render(
+    <ReactWindowGrid {...props} />
+  )
+  // console.log(prettyDOM(container))
+  expect(container).toMatchSnapshot()
+
+  const headers = getAllByText(/^Column \d$/)
+  expect(headers.length).toBe(2)
+  expect(getStyle(headers[0])).toMatch(/width: 20px;/)
+  expect(getStyle(headers[1])).toMatch(/width: 16px;/)
+
+  const rowHeaders = getAllByText(/^\d$/)
+  expect(rowHeaders.length).toBe(3)
+
+  let cells = getAllByText(/^cell/)
+  expect(cells.length).toBe(6)
+  expect(getStyle(cells[0])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[1])).toMatch(/width: 16px;/)
+  expect(getStyle(cells[2])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[3])).toMatch(/width: 16px;/)
+  expect(getStyle(cells[4])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[5])).toMatch(/width: 16px;/)
+
+  const {height, ...rest} = props
+  rerender(<ReactWindowGrid {...rest} maxHeight={39} enablePivot />)
+  cells = getAllByText(/^cell/)
+  expect(cells.length).toBe(6)
+  expect(getStyle(cells[0])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[1])).toMatch(/width: 16px;/)
+  expect(getStyle(cells[2])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[3])).toMatch(/width: 16px;/)
+  expect(getStyle(cells[4])).toMatch(/width: 20px;/)
+  expect(getStyle(cells[5])).toMatch(/width: 16px;/)
 })
